7033cb6044f657d296da70f5046fc78ede61fd65
yemrealtanay <y.emrealtanay@gmail.com>
2021-06-22 16:27:08 +0300

    Container eklendi

--- web_sitem/index.php
+++ web_sitem/index.php
@@ -11,7 +11,19 @@
     <title>Yunus Emre Altanay</title>
   </head>
   <body>
-    <h1>Siteme Hoşgeldiniz</h1>
+    <div class="container-lg">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
